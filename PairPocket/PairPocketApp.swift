7b3ffb9a7413c1f69db481e846376eed4f393882
Jangsu KIm <jangsoo719@gmail.com>
2026-03-02 17:58:49 +0900

    Initial Commit

---
+++ PairPocket/PairPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  PairPocketApp.swift
+//  PairPocket
+//
+//  Created by Jangsoo Kim on 2026/03/02.
+//
+
+import SwiftUI
+
+@main
+struct PairPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
